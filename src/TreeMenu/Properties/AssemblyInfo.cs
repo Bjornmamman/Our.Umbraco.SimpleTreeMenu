133cb5487edec214edf21a71ca203549cd592e4f
abi <abi@novicell.co.uk>
2019-12-03 09:12:20 +0000

    ignore Umbraco, change SimpleTreeMenuConverter to allow to use correct type in ModelsBuilder, correct Package.config to not use UmbracoCMS but only UmbracoCMS.Core

---
+++ src/TreeMenu/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Our.Umbraco.SimpleTreeMenu")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Björnmamman")]
+[assembly: AssemblyProduct("Our.Umbraco.SimpleTreeMenu.Properties")]
+[assembly: AssemblyCopyright("Copyright ©2019 Björnmamman AB")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("72a9a37d-72d0-4205-ad32-d991baf61a52")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.2.0")]

44a051485b06d8f8223e2da0b0f1156a25e7944d
Dennis Öhman <dennis@bjornmman.se>
2020-04-29 13:41:24 +0200

    Dropdown doctypepicker for datatype

---
+++ src/TreeMenu/App_Plugins/SimpleTreeMenu/Js/doctypepicker.controller.js
@@ -0,0 +1,21 @@
+﻿angular.module("umbraco").controller('SimpleTreeMenu.DoctypePicker', function ($scope, contentTypeResource) {
+    var vm = this;
+
+    vm.items = [];
+
+    contentTypeResource.getAll().then(function (data) {
+    
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].isElement === true)
+            {
+                vm.items.push(data[i]);
+            }
+        }
+
+        if (($scope.model.value === "" || $scope.model.value === "undefined") && vm.items.length > 0) {
+            $scope.model.value = vm.items[0].alias;
+        }
+    });
+
+
+});

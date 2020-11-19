6d062a295b4ef0e360c0b2c9cd81631b704b6fb0
Dennis Öhman <dennis@bjornmman.se>
2020-11-19 13:24:09 +0100

    Pump version 1.1

--- build/build.bat
+++ build/build.bat
@@ -1,3 +1,3 @@
 ::dotnet pack ..\src\TreeMenu.sln -o "..\release" -c release -p:Version=0.4
-nuget pack package.nuspec -OutputDirectory "..\release" -Version 1.0
-umbpack pack .\package.xml -o "..\release" -v 1.0
+nuget pack package.nuspec -OutputDirectory "..\release" -Version 1.1
+umbpack pack .\package.xml -o "..\release" -v 1.1

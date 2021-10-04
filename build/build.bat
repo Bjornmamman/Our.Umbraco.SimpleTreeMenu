e961d23465c4ec6fb706dc48cc4ea9dd46e89138
Dennis Öhman <dennis.ohman@bjornmamman.se>
2021-10-04 14:10:08 +0200

    Move files around

--- build/build.bat
+++
@@ -1,3 +0,0 @@
-::dotnet pack ..\src\TreeMenu.sln -o "..\release" -c release -p:Version=0.4
-nuget pack package.nuspec -OutputDirectory "..\release" -Version 1.1
-umbpack pack .\package.xml -o "..\release" -v 1.1
